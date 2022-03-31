5785dc67c987cc70455a318b9bc932c73ebb1db7
Matthew Carlson <carlson.matt.r@gmail.com>
2022-03-31 16:29:42 -0700

    making progress, able to get capital letters

--- script.js
+++ script.js
@@ -1,23 +1,36 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-var lowerCase = "abcdefghijklmnopqrstuvwxyz";
-var chooseLowerCase = lowerCase[Math.floor(Math.random() * lowerCase.length)];
 
-var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-var characters = "`~!@#$%^&*()=+]}[{;:/?.>,<";
 
 function generatePassword() {
+  var lowerCase = "abcdefghijklmnopqrstuvwxyz";
+  var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  var numbers = "0123456789";
+  var characters = "`~!@#$%^&*()=+]}[{;:/?.>,<";
   var password = "";
-  var userInput = Number(prompt("How long do you want your password to be?"));
-  if (userInput <= 5 || userInput > 128) {
+  var length= 0;
+  var output= "";
+  var chooseLower = false;
+  var chooseNumber = false;
+  
+  var length = prompt("How long do you want your password to be?");
+  if (length < 5 || length > 128) {
     alert("Must pick a number between 5 and 128!");
+    return("");
   }
-  else {
-    alert("Great!");
+  var chooseUpper = confirm("Do you want uppercase letters?");
+  if(chooseUpper = true){
+    output = upperCase; 
+    console.log("Choose upper");
+    console.log(output);
   }
-
+  else{
+    output = ("");
+    console.log("none");
+  }
+  
+  password=output.repeat(length);
 
   // TODO: add code to generate the password here
 
@@ -34,3 +47,15 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+// 0) setup repo and commit starter code and deploy
+// 1) prompt for password length
+// 2) generate pw all "a"'s for the desired length
+// 3) display all random lowercase letters for the chose length
+// 4) display all random uppercase/lowercase letters
+// 5) let user choose to include uppercase and/or lowercase
+// 6) let user choose upper, lower, and/or numbers
+// 7) let user choose upper, lower, numbers, and/or specials
+// 8) prevent user from too short
+// 9) prevent user from too short or too long
+// 10) prevent user from choosing no character types
