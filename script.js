180a444654268a3d83344677d032f2135e76b454
Matthew Carlson <carlson.matt.r@gmail.com>
2022-03-31 09:26:07 -0700

    asks for a number and tells you if its okay

--- script.js
+++ script.js
@@ -5,40 +5,30 @@ var lowerCase = "abcdefghijklmnopqrstuvwxyz";
 var chooseLowerCase = lowerCase[Math.floor(Math.random() * lowerCase.length)];
 
 var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-var numbers = [0,1,2,3,4,5,6,7,8,9];
+var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 var characters = "`~!@#$%^&*()=+]}[{;:/?.>,<";
 
 function generatePassword() {
-    var userInput = Number(prompt("How long do you want your password to be?"));
-    if(userInput.length <= 5 && userInput.length > 128){
-     alert("Great!");
-    }
-    else{
-    alert("Must pick a number between 5 and 128");
-    }
-    var password = lowerCase.length;
-    var lowerCase = "abcdefghijklmnopqrstuvwxyz";
-    var lowerCase = lowerCase[Math.floor(Math.random() * lowerCase.length)];
+  var password = "";
+  var userInput = Number(prompt("How long do you want your password to be?"));
+  if (userInput <= 5 || userInput > 128) {
+    alert("Must pick a number between 5 and 128!");
+  }
+  else {
+    alert("Great!");
+  }
 
 
   // TODO: add code to generate the password here
-  var upper = confirm("Would you like upper case letters?");
-  if (upper === true){
-    userInput.length
-  }
-  var chooseLowerCase = confirm("Do you want to include lowercase letters?");
-  var numbers = confirm("Do you want numbers?");
-  var chooseCharacters = confirm("Do you want special characters?");
 
   return password;
 }
-generatePassword();
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
-  passwordText.value = "aaaaaa";
+  passwordText.value = password;
 
 }
 
