75b44dfac175d28884018b214afd258b4ecb0388
Matthew Carlson <carlson.matt.r@gmail.com>
2022-03-31 21:26:04 -0700

    final deploy

---
+++ README.md
@@ -0,0 +1,21 @@
+#<password-creator-secure>
+
+## Description-
+This project is a password generator with multiple content character options to choose from.  In today's technology age, password security is paramount.  The app streamlines the creations  of new passwords by creating one for the user at the click of a button.
+
+JavaScript is a tough language, and I definitely had to push myself.  The hardest part was implementing a formula to randomize the characters.  Noting the proper syntax and formation of the formula was a challenge, but it felt really good once it was implemented.  
+
+In addition, I think this assignment forced me to be patient.  This was a challenge at times as I do like feeling like a go getter with assignments.  But slowing down helped in the long term for this project.  
+
+## Contributors-
+I worked with my classmate Sam, we helped each other with the syntax formation and looking for other ways to complete the program's requirements.  I also received help from my instructor John and TA Michael.
+
+## Testing-
+If you would like to try the program, you can find the link here at https://mrmrc182.github.io/password-creator-secure/ 
+
+Hit the red "generate password" as seen on the red button in the screenshot below.  You the user will be asked to input the length of your desired password.  Make sure it is from 5-128.  You will then be asked if you would like to include lowercase letters, uppercase letters, numbers, or special characters.  You must say "Yes" to at least one.  
+
+![Hit the red button for a great new password!](pwgscreenshot.png)
+
+## License-
+Matt Carlson Code 2022
--- script.js
+++ script.js
@@ -4,57 +4,50 @@ var lowerCase = "abcdefghijklmnopqrstuvwxyz";
   var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
   var numbers = "0123456789";
   var characters = "`~!@#$%^&*()=+]}[{;:/?.>,<";
-  // var options= 0;
+  var options= 0;
   var output= "";
   var chooseUpper= false;
   var chooseLower = false;
   var chooseNumber = false;
   var chooseChar = false;
   var userLength = 0;
+  var letters = 0;
 
 
 function generatePassword() {
   var password = " ";
-  var userLength = prompt("How long do you want your password to be?");
+ userLength = prompt("How long do you want your password to be?");
   if (userLength < 5 || userLength > 128) {
     alert("Must pick a number between 5 and 128!");
     return "";
   }
   else{
     options = userLength;
-    console.log(options);
   }
+  
   var chooseLower = confirm("Do you want lowercase letters?");
   if (chooseLower === true){
     output = output + lowerCase;
-    console.log(output);
   }
-
-  var chooseNumber = confirm("Do you want numbers?");
-  if (chooseNumber === true){
-    output = output + numbers; 
-    console.log("Choose number");
-    console.log(output);
-  }
-  
-  var chooseChar = confirm("Do you want special characters?");
-  if (chooseChar === true){
-    output = output + characters; 
-    console.log("Choose character");
-    console.log(output);
-  }
-
   var chooseUpper = confirm("Do you want uppercase letters?");
   if (chooseUpper === true){
     output = output + upperCase; 
-    console.log("Choose upper");
-    console.log(output);
   }
+  var chooseNumber = confirm("Do you want numbers?");
+  if (chooseNumber === true){
+    output = output + numbers; 
+  }
+  var chooseChar = confirm("Do you want special characters?");
+  if (chooseChar === true){
+    output = output + characters; 
+  }
+
+  
   if(output === ""){
     alert("You must choose one option");
   }
-  for (var i = 0; i < userLength; i++){
-    password = password + output[Math.floor(Math.random() * userLength)];
+  for (var i = 0; i < options; i++){
+    password = password + output[Math.floor(Math.random() * output.length)];
   }
 
   return password;
@@ -73,14 +66,4 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-// 0) setup repo and commit starter code and deploy
-// 1) prompt for password length
-// 2) generate pw all "a"'s for the desired length
-// 3) display all random lowercase letters for the chose length
-// 4) display all random uppercase/lowercase letters
-// 5) let user choose to include uppercase and/or lowercase
-// 6) let user choose upper, lower, and/or numbers
-// 7) let user choose upper, lower, numbers, and/or specials
-// 8) prevent user from too short
-// 9) prevent user from too short or too long
-// 10) prevent user from choosing no character types
+
