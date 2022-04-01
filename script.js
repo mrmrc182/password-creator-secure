801f3ada944d7c8c5bb81aabf7b5145c0361bb64
Matthew Carlson <carlson.matt.r@gmail.com>
2022-03-31 19:42:42 -0700

    got it to randomize.  now just have to put one of each

--- script.js
+++ script.js
@@ -1,76 +1,66 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-
-
-
-function generatePassword() {
-  var lowerCase = "abcdefghijklmnopqrstuvwxyz";
+var lowerCase = "abcdefghijklmnopqrstuvwxyz";
   var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
   var numbers = "0123456789";
   var characters = "`~!@#$%^&*()=+]}[{;:/?.>,<";
-  var password = "";
-  var length= 0;
+  // var options= 0;
   var output= "";
   var chooseUpper= false;
   var chooseLower = false;
   var chooseNumber = false;
   var chooseChar = false;
-  
-  var length = prompt("How long do you want your password to be?");
-  if (length < 5 || length > 128) {
+  var userLength = 0;
+
+
+function generatePassword() {
+  var password = " ";
+  var userLength = prompt("How long do you want your password to be?");
+  if (userLength < 5 || userLength > 128) {
     alert("Must pick a number between 5 and 128!");
-    return("");
-  }
-  var chooseUpper = confirm("Do you want uppercase letters?");
-  if(chooseUpper === true){
-    output = upperCase; 
-    console.log("Choose upper");
-    console.log(output);
+    return "";
   }
   else{
-    output = "";
-    console.log(output);
+    options = userLength;
+    console.log(options);
   }
   var chooseLower = confirm("Do you want lowercase letters?");
-  if(chooseLower === true){
-    output = lowerCase; 
-    console.log("Choose lower");
+  if (chooseLower === true){
+    output = output + lowerCase;
     console.log(output);
   }
-  else{
-    output = "";
-    console.log(output);
-  }
+
   var chooseNumber = confirm("Do you want numbers?");
-  if(chooseNumber === true){
-    output = numbers; 
+  if (chooseNumber === true){
+    output = output + numbers; 
     console.log("Choose number");
     console.log(output);
   }
-  else{
-    output = "";
-    console.log(output);
-  }
+  
   var chooseChar = confirm("Do you want special characters?");
-  if(chooseChar === true){
-    output = characters; 
+  if (chooseChar === true){
+    output = output + characters; 
     console.log("Choose character");
     console.log(output);
   }
-  else{
-    output = "";
+
+  var chooseUpper = confirm("Do you want uppercase letters?");
+  if (chooseUpper === true){
+    output = output + upperCase; 
+    console.log("Choose upper");
     console.log(output);
   }
   if(output === ""){
     alert("You must choose one option");
   }
-  
-  password=output;
-
-  // TODO: add code to generate the password here
+  for (var i = 0; i < userLength; i++){
+    password = password + output[Math.floor(Math.random() * userLength)];
+  }
 
   return password;
 }
+  // TODO: add code to generate the password here
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
